ef624f7dcdb98ff7350f7195f42f3c4756a12b0b
Freeman Wang <shuaichengwang@gmail.com>
2019-09-01 04:50:18 +0100

    update fishrc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -47,6 +47,24 @@ alias drm='docker rm '
 alias dstart='docker start '
 alias dstop='docker stop '
 
+# function
+function f
+    echo f
+    ls -ahF --color=auto -l | grep --color=auto $argv
+end
+
+function f1
+    echo f
+    ls -ahF --color=auto | grep --color=auto $argv
+end
+
+function hg
+    history | grep $argv | grep -v hsearch;
+end
+
+function md
+    mkdir $argv; chmod g+w $argv
+end
 
 # # # alias {{{
 # #-------------------
