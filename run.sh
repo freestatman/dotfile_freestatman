a456b14f9950a30d1a950cf1abf10678917a8e24
Freeman S. Wang <1614226+freestatman@users.noreply.github.com>
2020-03-03 22:41:54 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 
 cd ~/github/dotfile_swa
 mkdir -p ~/.config/fish/
+mkdir -p ~/.vim/plugged/vim-snippets/snippets/
 
 ln -f .vimrc ~/
 ln -f .bashrc ~/
+ln -f .git-prompt.bash ~/
 ln -f .tmux.conf ~/
 ln -f .config/fish/config.fish ~/.config/fish/config.fish
-
+ln -f .vim/snippets/r.snippets ~/.vim/plugged/vim-snippets/snippets/
